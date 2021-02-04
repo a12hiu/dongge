2dcd2ac2e59c689881c281ea77ca670034c47190
a12hiu <a12hiu@163.com>
2021-02-04 17:35:39 +0800

    ssss

---
+++ scripts/test_allure.py
@@ -0,0 +1,13 @@
+class TestAllure:
+
+    def test_1(self):
+        print('真')
+
+    def test_2(self):
+        print('假')
+
+    def test_3(self):
+        print('你好')
+
+    def test_4(self):
+        print('我是中国人')
